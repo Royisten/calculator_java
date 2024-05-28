ceb8c6d2d5a21a96abbf6038651c39497b2e7d1d
royisten <nemihidomine@gmail.com>
2024-05-28 11:03:02 +0530

    28/5/24 3

--- cal01/test.java
+++ cal01/test.java
@@ -1,7 +1,7 @@
 package cal01;
 public class test {
     public static void main(String[] args) {
-        System.out.println("Philippians 4:19 says,.... \"And my God will meet all your needs according to the riches of his glory in Christ Jesus\"");
+        System.out.println("Philippians 4:19 says, \"And my God will meet all your needs according to the riches of his glory in Christ Jesus\"");
     }
 }
 
